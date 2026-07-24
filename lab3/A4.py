3e138cc014a3f1ba65a40b32a44c01fb0df086e4
JahnaviSatish <jahnavisatish29@gmail.com>
2026-07-24 20:13:03 +0530

    lab3,A4 second commit

--- lab3/A4.py
+++ lab3/A4.py
@@ -1,17 +1,38 @@
-import pandas as pd
 import numpy as np
-from scipy.spatial import distance
-import random
 def minkowski_dist(v1,v2,p):
     dist=0
     for i,j in zip(v1,v2):
         dist+=(abs(i-j))**p
-    dist=dist/p
+    dist=dist**(1/p)
     return dist
-#def distance_classification(p)
+def euclidean(v1,v2):
+    euclidean=0
+    for i,j in zip(v1,v2):
+        euclidean+=(j-i)**2
+    euclidean=np.sqrt(euclidean)
+    return euclidean
+def manhattan(v1,v2):
+    manhattan=0
+    for i,j in zip(v1,v2):
+        manhattan+=abs(i-j)
+    return manhattan
+# note to self- taken from w3schools
+# The zip() function returns a zip object, which is an iterator of tuples 
+# where the first item in each passed iterator is paired together, 
+# and then the second item in each passed iterator are paired together etc.
+# If the passed iterables have different lengths, the iterable with the least items 
+# decides the length of the new iterator.
 A=[1,0,0]
 B=[2,4,0]
-p=random.randint(1,3)
-minkowski=minkowski_dist(A,B,p)
-print("minkowski dist=",minkowski)
-print(p)
+p = int(input("Enter order p>0: "))
+minkowski=minkowski_dist(A,B,p)        
+print("minkowski distance=",minkowski)
+print("order value=",p)
+if p==1:# manhatten is always order 1
+    m=manhattan(A,B)
+    print("manhatten distance=",m)
+elif p==2:#euclidean order 2
+    e=euclidean(A,B)
+    print("euclidean distance=",e)
+else:
+    print("neither manhattan nor euclidean as p is greater than order 2")
